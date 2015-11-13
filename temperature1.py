0afd4c0d27e6ed2ed057f1510150f8021468d698
karthikbnd <karthikbandi1991@gmail.com>
2015-11-13 11:23:15 +0100

    add

--- temperature1.py
+++ temperature1.py
@@ -8,7 +8,7 @@ ADC.setup()
  
 while True:
     reading = ADC.read(sensor_pin)
-    millivolts = reading * 1800  # 1.8V reference = 1800 mV
+    millivolts = (reading / 4096) * 1800  # 1.8V reference = 1800 mV
     temp_c = (millivolts - 500) / 10
     temp_f = (temp_c * 9/5) + 32
     print('mv=%d C=%d F=%d' % (millivolts, temp_c, temp_f))
